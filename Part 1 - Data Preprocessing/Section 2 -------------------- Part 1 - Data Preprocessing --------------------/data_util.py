5abda1c8fffaa8a3f880157d9c2a6883ff1f0f44
kbetlej <krzysztof.betlej@tivo.com>
2018-11-27 17:46:54 +0100

    simple regression

---
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_util.py
@@ -0,0 +1,15 @@
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_y = LabelEncoder()
+y = labelencoder_X.fit_transform(y)
---
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.R
@@ -0,0 +1,45 @@
+# Simple Linear Regression
+
+# Importing the dataset
+dataset = read.csv('Salary_Data.csv')
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Fitting Simple Linear Regression to the training set
+regressor = lm(formula = Salary ~ YearsExperience, data = training_set)
+
+# Predicting the Test set results
+y_pred = predict(regressor, newdata = test_set)
+
+# Visualize the Training set results
+# uncomment to install missing library
+# install.packages('ggplot2')
+library(ggplot2)
+
+ggplot() +
+  geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary), 
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Salary vs Experience (training set)') +
+  xlab('Year of experience') +
+  ylab('Salary')
+
+# Visualize the Test set results
+
+ggplot() +
+  geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary), 
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Salary vs Experience (training set)') +
+  xlab('Year of experience') +
+  ylab('Salary')
+
+
